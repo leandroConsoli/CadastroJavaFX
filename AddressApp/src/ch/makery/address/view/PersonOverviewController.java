9317172e45ea5ec3ee0ca3b9a91507487daf0e18
Leandro Consoli <lgconsoli@univel.br>
2016-03-24 14:32:49 -0300

    Correção do método showPersonDetails.

--- AddressApp/src/ch/makery/address/view/PersonOverviewController.java
+++ AddressApp/src/ch/makery/address/view/PersonOverviewController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import ch.makery.address.MainApp;
 import ch.makery.address.model.Person;
+import ch.makery.address.util.DateUtil;
 
 public class PersonOverviewController {
 	@FXML
@@ -43,9 +44,18 @@ public class PersonOverviewController {
 	 */
 	@FXML
 	private void initialize() {
-		// Inicializa a tablea de pessoa com duas colunas.
-		firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
-		lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+	    // Inicializa a tabela de pessoas com duas colunas.
+	    firstNameColumn.setCellValueFactory(
+	            cellData -> cellData.getValue().firstNameProperty());
+	    lastNameColumn.setCellValueFactory(
+	            cellData -> cellData.getValue().lastNameProperty());
+
+	    // Limpa os detalhes da pessoa.
+	    showPersonDetails(null);
+
+	    // Detecta mudanças de seleção e mostra os detalhes da pessoa quando houver mudança.
+	    personTable.getSelectionModel().selectedItemProperty().addListener(
+	            (observable, oldValue, newValue) -> showPersonDetails(newValue));
 	}
 
 	/**
@@ -69,25 +79,22 @@ public class PersonOverviewController {
 	 *            a pessoa ou null
 	 */
 	private void showPersonDetails(Person person) {
-		if (person != null) {
-			// Preenche as labels com informações do objeto person.
-			firstNameLabel.setText(person.getFirstName());
-			lastNameLabel.setText(person.getLastName());
-			streetLabel.setText(person.getStreet());
-			postalCodeLabel.setText(Integer.toString(person.getPostalCode()));
-			cityLabel.setText(person.getCity());
-
-			// TODO: Nós precisamos de uma maneira de converter o aniversário em
-			// um String!
-			// birthdayLabel.setText(...);
-		} else {
-			// Person é null, remove todo o texto.
-			firstNameLabel.setText("");
-			lastNameLabel.setText("");
-			streetLabel.setText("");
-			postalCodeLabel.setText("");
-			cityLabel.setText("");
-			birthdayLabel.setText("");
-		}
-	}
+    	if (person != null) {
+    		// Fill the labels with info from the person object.
+    		firstNameLabel.setText(person.getFirstName());
+    		lastNameLabel.setText(person.getLastName());
+    		streetLabel.setText(person.getStreet());
+    		postalCodeLabel.setText(Integer.toString(person.getPostalCode()));
+    		cityLabel.setText(person.getCity());
+    		birthdayLabel.setText(DateUtil.format(person.getBirthday()));
+    	} else {
+    		// Person is null, remove all the text.
+    		firstNameLabel.setText("");
+    		lastNameLabel.setText("");
+    		streetLabel.setText("");
+    		postalCodeLabel.setText("");
+    		cityLabel.setText("");
+    		birthdayLabel.setText("");
+    	}
+    }
 }
