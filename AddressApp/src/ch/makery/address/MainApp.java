f2725e149bc84d61a608fe47e42125076215d2d3
Leandro Consoli <lgconsoli@univel.br>
2016-03-24 17:27:21 -0300

    Métodos que tomam conta de salvar e recuperar as preferences.

--- AddressApp/src/ch/makery/address/MainApp.java
+++ AddressApp/src/ch/makery/address/MainApp.java
@@ -1,6 +1,8 @@
 package ch.makery.address;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.prefs.Preferences;
 
 import javafx.application.Application;
 import javafx.collections.FXCollections;
@@ -57,15 +59,15 @@ public class MainApp extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-	    this.primaryStage = primaryStage;
-	    this.primaryStage.setTitle("AddressApp");
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("AddressApp");
 
-	    // Set the application icon.
-	    this.primaryStage.getIcons().add(new Image("file:resources/images/address_book_32.png"));
+		// Set the application icon.
+		this.primaryStage.getIcons().add(new Image("file:resources/images/address_book_32.png"));
 
-	    initRootLayout();
+		initRootLayout();
 
-	    showPersonOverview();
+		showPersonOverview();
 	}
 
 	/**
@@ -121,42 +123,84 @@ public class MainApp extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
+
 	/**
-	 * Abre uma janela para editar detalhes para a pessoa especificada. Se o usuário clicar
-	 * OK, as mudanças são salvasno objeto pessoa fornecido e retorna true.
+	 * Abre uma janela para editar detalhes para a pessoa especificada. Se o
+	 * usuário clicar OK, as mudanças são salvasno objeto pessoa fornecido e
+	 * retorna true.
 	 * 
-	 * @param person O objeto pessoa a ser editado
-	 * @return true Se o usuário clicou OK,  caso contrário false.
+	 * @param person
+	 *            O objeto pessoa a ser editado
+	 * @return true Se o usuário clicou OK, caso contrário false.
 	 */
 	public boolean showPersonEditDialog(Person person) {
-	    try {
-	        // Carrega o arquivo fxml e cria um novo stage para a janela popup.
-	        FXMLLoader loader = new FXMLLoader();
-	        loader.setLocation(MainApp.class.getResource("view/PersonEditDialog.fxml"));
-	        AnchorPane page = (AnchorPane) loader.load();
+		try {
+			// Carrega o arquivo fxml e cria um novo stage para a janela popup.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/PersonEditDialog.fxml"));
+			AnchorPane page = (AnchorPane) loader.load();
 
-	        // Cria o palco dialogStage.
-	        Stage dialogStage = new Stage();
-	        dialogStage.setTitle("Edit Person");
-	        dialogStage.initModality(Modality.WINDOW_MODAL);
-	        dialogStage.initOwner(primaryStage);
-	        Scene scene = new Scene(page);
-	        dialogStage.setScene(scene);
+			// Cria o palco dialogStage.
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Edit Person");
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.initOwner(primaryStage);
+			Scene scene = new Scene(page);
+			dialogStage.setScene(scene);
 
-	        // Define a pessoa no controller.
-	        PersonEditDialogController controller = loader.getController();
-	        controller.setDialogStage(dialogStage);
-	        controller.setPerson(person);
+			// Define a pessoa no controller.
+			PersonEditDialogController controller = loader.getController();
+			controller.setDialogStage(dialogStage);
+			controller.setPerson(person);
 
-	        // Mostra a janela e espera até o usuário fechar.
-	        dialogStage.showAndWait();
+			// Mostra a janela e espera até o usuário fechar.
+			dialogStage.showAndWait();
 
-	        return controller.isOkClicked();
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	        return false;
-	    }
+			return controller.isOkClicked();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
-	
+
+	/**
+	 * Retorna o arquivo de preferências da pessoa, o último arquivo que foi
+	 * aberto. As preferências são lidas do registro específico do SO (Sistema
+	 * Operacional). Se tais prefêrencias não puderem ser encontradas, ele
+	 * retorna null.
+	 * 
+	 * @return
+	 */
+	public File getPersonFilePath() {
+		Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+		String filePath = prefs.get("filePath", null);
+		if (filePath != null) {
+			return new File(filePath);
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * Define o caminho do arquivo do arquivo carregado atual. O caminho é
+	 * persistido no registro específico do SO (Sistema Operacional).
+	 * 
+	 * @param file
+	 *            O arquivo ou null para remover o caminho
+	 */
+	public void setPersonFilePath(File file) {
+		Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+		if (file != null) {
+			prefs.put("filePath", file.getPath());
+
+			// Update the stage title.
+			primaryStage.setTitle("AddressApp - " + file.getName());
+		} else {
+			prefs.remove("filePath");
+
+			// Update the stage title.
+			primaryStage.setTitle("AddressApp");
+		}
+	}
+
 }
